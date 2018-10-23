e18edb487fe7988c446e57d586f524df9d22029c
shumengma <shumeng.ma@qunar.com>
2018-10-23 16:48:05 +0800

    okhttp 拦截器

--- MyApplication/app/src/main/java/com/example/ma/testapp/OkHttpActivity.java
+++ MyApplication/app/src/main/java/com/example/ma/testapp/OkHttpActivity.java
@@ -3,6 +3,7 @@ package com.example.ma.testapp;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -15,10 +16,15 @@ import okhttp3.Call;
 import okhttp3.Callback;
 import okhttp3.FormBody;
 import okhttp3.Headers;
+import okhttp3.Interceptor;
+import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.RequestBody;
 import okhttp3.Response;
+import okio.BufferedSink;
+import okio.GzipSink;
+import okio.Okio;
 
 /**
  * Created by shumengma on 2018/10/18.
@@ -32,7 +38,12 @@ public class OkHttpActivity extends AppCompatActivity {
 
 //    private static final String SERVER_URL = "http://kbx.chuyunspace.com/router";
     private static final String WEATHER_SERVER_URL = "http://www.weather.com.cn/data/sk/101110101.html";
-    private OkHttpClient okHttpClient = new OkHttpClient();
+    private OkHttpClient okHttpClient = new OkHttpClient
+                                            .Builder()
+                                            .addInterceptor(new LogginInterceptor()) //添加应用拦截器
+                                            .addInterceptor(new GzipRequestInterceptor())
+//                                            .addInterceptor(REWRITE_CACHE_CONTROL_INTERCEPTOR)
+                                            .build();
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -88,4 +99,67 @@ public class OkHttpActivity extends AppCompatActivity {
         });
         t.start();
     }
+    //拦截器，添加了日志打印
+    class LogginInterceptor implements Interceptor{
+        @Override
+        public Response intercept(Chain chain) throws IOException {
+            Request request = chain.request();
+            long t1 = System.nanoTime();
+            Log.d("------", "intercept: Sending request "+ request.url() + " on " + chain.connection() + "\n "+ request.headers());
+
+            Response response = chain.proceed(request);
+            long t2 = System.nanoTime();
+            Log.d("------", "intercept: Receive response from " + response.request().url() + " in " + (t2-t1) + "\n" + response.headers());
+            return response;
+        }
+    }
+    //请求体压缩拦截器
+    class GzipRequestInterceptor implements Interceptor{
+
+        @Override
+        public Response intercept(Chain chain) throws IOException {
+            Request originalRequest = chain.request();
+            Log.d("----", "intercept: encoding:"+originalRequest.headers("Content-Encoding"));
+            if (originalRequest.body() == null || originalRequest.headers("Content-Encoding") != null){
+                return chain.proceed(originalRequest);
+            }
+            Request compressedRequest = originalRequest.newBuilder()
+                    .header("Content-Encoding", "gzip")
+                    .method(originalRequest.method(), gzip(originalRequest.body()))
+                    .build();
+
+            return chain.proceed(compressedRequest);
+        }
+        private RequestBody gzip(final RequestBody body){
+            return new RequestBody() {
+                @Nullable
+                @Override
+                public MediaType contentType() {
+                    return body.contentType();
+                }
+
+                @Override
+                public long contentLength() throws IOException {
+                    return -1; //we don`t know the compressed length in advance
+                }
+
+                @Override
+                public void writeTo(BufferedSink sink) throws IOException {
+                    BufferedSink gzipSkin = Okio.buffer(new GzipSink(sink));
+                    body.writeTo(gzipSkin);
+                    gzipSkin.close();
+                }
+            };
+        }
+    }
+    //重写响应结果拦截器
+    private static final Interceptor REWRITE_CACHE_CONTROL_INTERCEPTOR = new Interceptor() {
+        @Override
+        public Response intercept(Chain chain) throws IOException {
+            Response originalResponse = chain.proceed(chain.request());
+            return originalResponse.newBuilder()
+                    .header("Cache-Control", "max-age=60")
+                    .build();
+        }
+    };
 }
